7091cc6108947f0be7c52b440a75337a8479b396
Eugenio <eugenioclrc@gmail.com>
2020-12-02 18:18:02 -0300

    i have to make this change to make tailwind work!

--- templates/postcss.config.js
+++ templates/postcss.config.js
@@ -3,10 +3,17 @@ const cssnano = require("cssnano");
 const mode = process.env.NODE_ENV;
 const dev = mode === "development";
 
-module.exports = {
-	plugins: [
-		!dev && cssnano({
-			preset: "default",
-		}),
-	],
+const plugins = {
+	tailwindcss: {},
+	autoprefixer: {},
 };
+
+if (!dev) {
+	plugins['cssnano'] = { preset: "default" };
+}
+// plugins: [
+// 	!dev && cssnano({
+// 		preset: "default",
+// 	}),
+
+module.exports = { plugins };
